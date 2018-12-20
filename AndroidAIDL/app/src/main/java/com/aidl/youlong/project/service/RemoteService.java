95ddec42831d374827b808befd366a70dfee0f3f
youlongxifeng <xie774505257@163.com>
2018-12-20 10:52:42 +0800

    Update RemoteService.java

--- AndroidAIDL/app/src/main/java/com/aidl/youlong/project/service/RemoteService.java
+++ AndroidAIDL/app/src/main/java/com/aidl/youlong/project/service/RemoteService.java
@@ -67,6 +67,13 @@ public class RemoteService extends Service {
             }
             return super.onTransact(code, data, reply, flags);
         }
+        
+         @Override
+        public void join(IBinder token, String name) throws RemoteException {
+            CustomerClient cl = new CustomerClient(token, name);
+            mClientsList.add(cl); 
+        }
+
 
         @Override
         public void addBook(Book book) throws RemoteException {
@@ -100,5 +107,31 @@ public class RemoteService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         return super.onStartCommand(intent, flags, startId);
     }
+    //这个list 就是用来存储当前餐厅有多少顾客 注意我们为什么没有用顾客的名字来存储？
+    //而是用了这个CustomerClient的类 看这个类的注释即可明白
+    private List<CustomerClient> mClientsList = new ArrayList<>();
+   //http://developer.android.com/intl/zh-cn/reference/android/os/Binder.html#linkToDeath(android.os.IBinder.DeathRecipient, int)
+    //实际上 这个接口 就是用来 当客户端自己发生崩溃时， 我们的服务端也能收到这个崩溃的消息
+    //并且会调用binderDied 这个回调方法，所以你看这个内部类的代码 就明白了 无非就是保证当客户端异常销毁的时候
+    //我们服务端也要保证收到这个消息 然后做出相应的应对
+    final class CustomerClient implements IBinder.DeathRecipient {
 
+        public final IBinder mToken;
+
+        public CustomerClient(IBinder mToken, String mCustomerName) {
+            this.mToken = mToken;
+            this.mCustomerName = mCustomerName;
+        }
+
+        public final String mCustomerName;
+
+        @Override
+        public void binderDied() {//粘结剂死亡
+            //我们的应对方法就是当客户端 也就是顾客异常消失的时候 我们要把这个list里面 的对象也移出掉
+            if (mClientsList.indexOf(this) >= 0) {
+                mClientsList.remove(this);
+            }
+
+        }
+    }
 }
